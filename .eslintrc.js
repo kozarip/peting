a1a6c3ef0d765b601dc3d50d34545a7fd7893f93
Peter Kozari <kozaripeti@gmail.com>
2020-04-04 00:07:32 +0200

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    "react/jsx-filename-extension": [2, { "extensions": [".js", ".jsx", ".ts", ".tsx"] }],
+    "indent": [2, 2],
+    "import/no-unresolved": 0,
+    "no-trailing-spaces": [1, { "skipBlankLines": true }],
+    "@typescript-eslint/no-use-before-define": 0,
+    "no-use-before-define": [2, { "variables": false }]
+  },
+};

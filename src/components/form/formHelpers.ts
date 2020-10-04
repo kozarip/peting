5311b1c71f4a52772be6823c36604eeb74379b86
Peter Kozari <peter.kozari@hbo.com>
2020-10-04 14:05:01 +0200

    fix: multiselector

--- src/components/form/formHelpers.ts
+++ src/components/form/formHelpers.ts
@@ -1,8 +1,11 @@
 export function createNewTypeObject(type, value) {
   let newValue;
   // eslint-disable-next-line no-restricted-globals
-  if (Array.isArray(value) && type === 'hobbies') {
-    newValue = value.map((v) => parseInt(v, 10));
+  if (Array.isArray(value)) {
+    if( type === 'hobbies'){
+      newValue = value.map((v) => parseInt(v, 10));
+    }
+    newValue = value;
   } else if (!isNaN(parseInt(value, 10))) {
     newValue = parseInt(value, 10);
   } else {

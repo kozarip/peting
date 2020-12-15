f26e7cd3c25697fb075a16bfc5aa18dc011f1d98
Peter Kozari <kozaripeti@gmail.com>
2020-12-15 14:27:42 +0100

    chore: new animal images

--- src/constants/animalImages.ts
+++ src/constants/animalImages.ts
@@ -9,10 +9,14 @@ const animalImages = {
   'guinea pig': require('../assets/images/animals/mouse.png'),
   rat: require('../assets/images/animals/mouse.png'),
   chinchilla: require('../assets/images/animals/mouse.png'),
+  hamster: require('../assets/images/animals/mouse.png'),
   degu: require('../assets/images/animals/mouse.png'),
   ferret: require('../assets/images/animals/ferret.jpg'),
+  rabbit: require('../assets/images/animals/ferret.jpg'),
   turtle: require('../assets/images/animals/turtle.png'),
   hedgehog: require('../assets/images/animals/hedgehog.jpg'),
+  lizard: require('../assets/images/animals/chameleon.jpg'),
+  dwarfPig: require('../assets/images/animals/pig.jpg'),
 };
 
 export default animalImages;
